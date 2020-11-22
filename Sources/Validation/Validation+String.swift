2957cf8e23533602bc1ef4550d71193a53c994f6
Sven Tiigi <sven.tiigi@gmail.com>
2020-11-22 22:17:02 +0100

    Removed Regular Expression Validations

--- Sources/Validation/Validation+String.swift
+++ Sources/Validation/Validation+String.swift
@@ -57,30 +57,6 @@ public extension Validation where Value: StringProtocol {
 
 public extension Validation where Value == String {
     
-    /// Validation with RegularExpression-Pattern
-    ///
-    /// - Parameters:
-    ///   - pattern: The RegularExpression String Pattern
-    ///   - options: The NSRegularExpression Options. Default value `.init`
-    ///   - matchingOptions: The NSRegularExpression MatchingOptions. Default value `.init`
-    /// - Returns: A Validation
-    static func regularExpression(
-        _ pattern: String,
-        options: NSRegularExpression.Options = .init(),
-        matchingOptions: NSRegularExpression.MatchingOptions = .init()
-    ) -> Validation {
-        // Verify NSRegularExpression can be constructed with String Pattern
-        guard let regularExpression = try? NSRegularExpression(pattern: pattern, options: options) else {
-            // Otherwise return false
-            return .init { _ in false }
-        }
-        // Return RegularExpression Validation
-        return self.regularExpression(
-            regularExpression,
-            matchingOptions: matchingOptions
-        )
-    }
-    
     /// Validation with RegularExpression
     ///
     /// - Parameters:
@@ -98,32 +74,6 @@ public extension Validation where Value == String {
                 range: .init(value.startIndex..., in: value)
             ) != nil
         }
-    }
-    
-    /// The Lowercased Validation
-    static var isLowercased: Validation {
-        return self.regularExpression("^.*?[a-z].*?$")
-    }
-    
-    /// The Uppercased Validation
-    static var isUppercased: Validation {
-        return self.regularExpression("^.*?[A-Z].*?$")
-    }
-    
-    /// The E-Mail Validation
-    static var isEmail: Validation {
-        // swiftlint:disable:next line_length
-        return self.regularExpression("^(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$")
-    }
-    
-    /// The URL Validation
-    static var isURL: Validation {
-        return self.regularExpression("(?i)https?://(?:www\\.)?\\S+(?:/|\\b)")
-    }
-    
-    /// The Numeric Validation
-    static var isNumeric: Validation {
-        return self.regularExpression("[0-9]+$")
     }
     
 }
