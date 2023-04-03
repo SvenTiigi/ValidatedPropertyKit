eee3dab9583541dc42da9278c3be6c750cf4e7b3
Jan Švec <jansvec92@gmail.com>
2023-04-03 11:57:27 +0200

    Add PublishedValidated wrapper and update example with usage

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -1,27 +1,44 @@
+import Combine
 import SwiftUI
 import ValidatedPropertyKit
 
+final class ContentModel: ObservableObject {
+    @PublishedValidated(.range(3...))
+    var username = "" {
+        didSet {
+            usernameInvalid = !_username.isValid
+            usernameInvalidAfterChanges = _username.isInvalidAfterChanges
+        }
+    }
+
+    @Published
+    var usernameInvalid = true
+
+    @Published
+    var usernameInvalidAfterChanges = false
+}
+
 struct ContentView {
-    
     @Validated(!.isEmpty)
     var username = String()
-    
+
+    @ObservedObject
+    var model = ContentModel()
+
+    @FocusState var focus
 }
 
 extension ContentView: View {
-    
     var body: some View {
         NavigationView {
             List {
                 Section(
                     header: Text(verbatim: "Username"),
                     footer: Group {
-                        if self._username.isInvalidAfterChanges {
-                            Text(
-                                verbatim: "Username is not valid"
-                            )
-                            .foregroundColor(.red)
-                        }
+                        Text(
+                            verbatim: _username.isInvalidAfterChanges ? "Username is not valid" : ""
+                        )
+                        .foregroundColor(.red)
                     }
                 ) {
                     TextField(
@@ -29,6 +46,7 @@ extension ContentView: View {
                         text: self.$username
                     )
                 }
+
                 Section(
                     footer: Button(
                         action: {
@@ -39,12 +57,36 @@ extension ContentView: View {
                     }
                     .buttonStyle(.borderedProminent)
                     .validated(self._username)
+                ) {}
+
+                Section(
+                    header: Text(verbatim: "View Model Username"),
+                    footer: Group {
+                        Text(
+                            verbatim: model.usernameInvalidAfterChanges ? "Username is not valid" : ""
+                        )
+                        .foregroundColor(.red)
+                    }
                 ) {
-                    
+                    TextField(
+                        "John Doe",
+                        text: $model.username
+                    )
                 }
+
+                Section(
+                    footer: Button(
+                        action: {
+                            print("Login")
+                        }
+                    ) {
+                        Text(verbatim: "Login")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .disabled(model.usernameInvalid)
+                ) {}
             }
             .navigationTitle("ValidatedPropertyKit")
         }
     }
-    
 }
---
+++ Sources/PublishedValidated.swift
@@ -0,0 +1,79 @@
+//
+//  PublishedValidated.swift
+//
+//
+//  Created by Jan Švec on 31.03.2023.
+//
+
+import Combine
+import SwiftUI
+
+@available(iOS 14.0, *)
+@propertyWrapper
+public final class PublishedValidated<Value>: ObservableObject, Validatable {
+    public var validation: Validation<Value> {
+        didSet {
+            validate()
+        }
+    }
+
+    @Published
+    public private(set) var value: Value
+
+    @Published
+    public private(set) var hasChanges = false
+
+    @Published
+    public private(set) var isValid: Bool
+
+    public var wrappedValue: Value {
+        get {
+            return value
+        }
+        set {
+            value = newValue
+            validate()
+
+            if !hasChanges {
+                hasChanges.toggle()
+            }
+        }
+    }
+
+    public var projectedValue: Published<Value>.Publisher {
+        get {
+            return $value
+        }
+        set {
+            $value = newValue
+            validate()
+
+            if !hasChanges {
+                hasChanges.toggle()
+            }
+        }
+    }
+
+    public var isInvalidAfterChanges: Bool {
+        hasChanges && !isValid
+    }
+
+    public func validate() {
+        isValid = validation.validate(value)
+    }
+
+    public init(
+        wrappedValue: Value,
+        _ validation: Validation<Value>
+    ) {
+        self.validation = validation
+
+        _value = .init(
+            initialValue: wrappedValue
+        )
+
+        _isValid = .init(
+            initialValue: validation.validate(wrappedValue)
+        )
+    }
+}
